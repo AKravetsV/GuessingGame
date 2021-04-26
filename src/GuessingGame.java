a7ab0354cd2c20fa85af4025ee7d8abd157007ba
Oleksandr K <kravetsalexandrv@gmail.com>
2021-04-26 12:10:30 +0300

    Initial commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,71 @@
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GuessingGame extends JFrame {
+    private JButton btnGuess;
+    private JTextField txtGuess;
+    private JLabel lblOutput;
+    private JPanel panelMain;
+    static int theNumber;
+
+    public GuessingGame() {
+
+        this.btnGuess.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                GuessingGame.this.checkGuess();
+            }
+        });
+        this.btnGuess.addKeyListener(new KeyAdapter() {
+        });
+        this.txtGuess.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                GuessingGame.this.checkGuess();
+            }
+        });
+    }
+
+    public void checkGuess() {
+        String guessText = this.txtGuess.getText();
+        String message = "";
+
+        try {
+            int guess = Integer.parseInt(guessText);
+            if (guess < theNumber) {
+                message = guess + " is too low. Try again.";
+            } else if (guess > theNumber) {
+                message = guess + " is too high. Try again.";
+            } else {
+                message = guess + " is correct. You win! Let's play again!";
+                newGame();
+            }
+        } catch (Exception var7) {
+            message = "Enter a whole number between 1 and 10.";
+        } finally {
+            this.lblOutput.setText(message);
+            this.txtGuess.requestFocus();
+            this.txtGuess.selectAll();
+        }
+
+    }
+
+    public static void newGame() {
+        theNumber = (int)(Math.random() * 10.0D + 1.0D);
+        System.out.println(theNumber);
+    }
+
+    public static void main(String[] args) {
+        GuessingGame theGame = new GuessingGame();
+        newGame();
+        theGame.setContentPane((new GuessingGame()).panelMain);
+        theGame.setDefaultCloseOperation(3);
+        theGame.setSize(new Dimension(550, 400));
+        theGame.setVisible(true);
+    }
+}
